bf2d21808c8233e30fa833be52a36700f46a3307
Lduf <lucas.dufour@insa-lyon.fr>
2021-11-26 22:07:45 +0100

    travail sur le file sender

--- src/server1.c
+++ src/server1.c
@@ -151,9 +151,9 @@ int send_file(int sockfd, struct sockaddr_in *client_addr, socklen_t client_addr
         memset(buffer, 0, sizeof(buffer));
         do{
             
-            buffer[i++] = fgetc(file);
+            buffer[i] = fgetc(file);
             printf("%c\n", buffer[i]);
-            
+            i++;
         }while(buffer[i-1] != EOF || i-1 < SEGMENT_SIZE);
         printf("Sending segment %s\n", buffer);
         /*
