98e9b917f528d12c96c86de702f7e9e6213843b5
Lduf <lucas.dufour@insa-lyon.fr>
2021-11-26 23:11:19 +0100

    test de debug *** stack smashing detected ***: terminated

--- src/server1.c
+++ src/server1.c
@@ -181,7 +181,8 @@ int send_file(int sockfd, struct sockaddr_in *client_addr, socklen_t client_addr
             printf("TIMEOUT !\n");
         }
         else{
-            acked = atoi(extract(ack_buffer, "ACK([0-9]{6})", 1));
+            //acked = atoi(extract(ack_buffer, "ACK([0-9]{6})", 1));
+            acked = packet_number;
             printf("ACK received for packet %06d\n", acked);
         }
     }while(flag_eof == 0);
