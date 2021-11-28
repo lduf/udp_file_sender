c9fce124d6ec4c719292ae65c9515af871e86817
Lduf <lucas.dufour@insa-lyon.fr>
2021-11-28 11:18:31 +0100

    tentative d'amélioration du timeout

--- src/server1.c
+++ src/server1.c
@@ -141,7 +141,7 @@ int send_file(int sockfd, struct sockaddr_in *client_addr, socklen_t client_addr
 
     acks = stack_push(acks, -1); // We push -1 to the stack, because we don't know the first ACK yet.
     acks->RTT = DEFAULT_RTT; // We initialize the RTT to 50.
-    segments = stack_push(segments, -2); // Because of the calculation method, we push -2 to the stack. So the first segment will be sent with sequence number 0.
+    segments = stack_push(segments, -1); // Because of the calculation method, we push -2 to the stack. So the first segment will be sent with sequence number 0.
 
 
     int packet_number = -1; // Packet number should be -1 
