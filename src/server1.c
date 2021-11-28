c74f29847e18db5f103a71d7f931b9264423e5f1
Lduf <lucas.dufour@insa-lyon.fr>
2021-11-28 11:14:00 +0100

    tentative d'amélioration du timeout

--- src/server1.c
+++ src/server1.c
@@ -221,7 +221,7 @@ int send_file(int sockfd, struct sockaddr_in *client_addr, socklen_t client_addr
         // Initialize the select
         FD_SET(sockfd, &readset);
         printf("estimated timeout : %d us\n",estimate_timeout(acks->RTT)); 
-        tv.tv_usec = estimate_timeout(acks->RTT); // Set the timeout based on the last received RTT.
+        tv.tv_usec = DEFAULT_TIMEOUT; //estimate_timeout(acks->RTT); // Set the timeout based on the last received RTT.
         char ack_buffer[16];
         memset(ack_buffer, 0, sizeof(ack_buffer));
 
