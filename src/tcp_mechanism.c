b798dd240a1c3806ea2e64cb4d6ff52880cb4516
lduf <lucas.dufour@insa-lyon.fr>
2021-12-13 22:07:04 +0100

    debug fenetre sans fin

--- src/tcp_mechanism.c
+++ src/tcp_mechanism.c
@@ -8,9 +8,9 @@ int swnd = 1; // fenêtre d'émission
 int awnd = 1; // fenêtre de réception
 int cwnd = DEFAULT_CWND; // fenêtre de congestion
 int ssthresh = 1024; // seuil de congestion slow start
-int FAST_RECOVERY_MOD = 1; // mode de récupération rapide
 int FAST_RETRANSMIT_MOD = 0; //mode fast retransmit
 int fast_rt_window  = DEFAULT_CWND; // fenêtre de congestion fast retransmit
+
 
 /**
  * @brief This function is used to give the next packet to send. It implement the fast retransmission mechanism.
@@ -64,7 +64,7 @@ int estimate_timeout(double rtt) {
     sRtt = (1 - ALPHA) * sRtt + ALPHA*rtt;
     devRtt = (1 - BETA) * devRtt + BETA * fabs(rtt - sRtt);
     //printf("Estimated RTT: %f us\n", sRtt);
-    return (sRtt + 4 * devRtt);
+    return (int) (sRtt + 4 * devRtt);
 }
 
 /**
@@ -103,7 +103,6 @@ void fast_retransmit() {
     FAST_RETRANSMIT_MOD = 1;
     fast_rt_window = cwnd;
     cwnd = DEFAULT_CWND;
-    FAST_RECOVERY_MOD = 1;
 }
 
 /**
@@ -149,7 +148,7 @@ void fast_recovery() {
          congestion_avoidance();
          printf("congestion_avoidance %d...\n", cwnd);
      }  
-     if (FAST_RECOVERY_MOD > 0) {
+     if (FAST_RECOVERY_MOD == 1 && FAST_RETRANSMIT_MOD == 1) {
          fast_recovery();
          printf("fast_recovery %d...\n", cwnd);
      }
