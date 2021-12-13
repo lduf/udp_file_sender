381bdadf0a01cc8e931b764f5ddae8b1a756714b
lduf <lucas.dufour@insa-lyon.fr>
2021-12-13 19:28:37 +0100

    tentative de fast retransmit

--- src/tcp_mechanism.c
+++ src/tcp_mechanism.c
@@ -9,6 +9,7 @@ int awnd = 1; // fenêtre de réception
 int cwnd = DEFAULT_CWND; // fenêtre de congestion
 int ssthresh = 1024; // seuil de congestion slow start
 int FAST_RETRANSMIT_MOD = 0; //mode fast retransmit
+int fast_rt_window  = DEFAULT_CWND; // fenêtre de congestion fast retransmit
 
 
 /**
@@ -100,6 +101,7 @@ void congestion_avoidance() {
 */
 void fast_retransmit() {
     FAST_RETRANSMIT_MOD = 1;
+    fast_rt_window = cwnd;
     cwnd = DEFAULT_CWND;
 }
 
@@ -110,7 +112,7 @@ void fast_retransmit() {
  */
 void fast_recovery() {
     if(FAST_RECOVERY_MOD == 1){
-
+        cwnd = (fast_rt_window / 2 >= DEFAULT_CWND) ? fast_rt_window / 2 : DEFAULT_CWND;
     }
     else{
         cwnd = DEFAULT_CWND;
