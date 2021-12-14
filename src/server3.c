ace463bf56193c82b55c5f4d49704864d0b04c56
lduf <lucas.dufour@insa-lyon.fr>
2021-12-14 17:39:57 +0100

    premiere tentative server3

--- src/server3.c
+++ src/server3.c
@@ -433,6 +433,7 @@ int main(int argc, char *argv[]) {
 
                 //end the session
                 end_connection(sockfd, &client_addr, client_addr_len);
+                close(new_sockfd);
             }
     }
     close(sockfd);
